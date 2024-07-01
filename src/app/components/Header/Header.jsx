81bf88b981ace6e4334ef04bcbc1c4edc5b2eb8c
Vipin <vipin.gopinath@avantgarde-partner.de>
2024-07-01 12:01:23 +0530

    initial commit

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./header.scss";
+
+export default function Header({ children }) {
+  return (
+    <header>
+        Header
+    </header>
+  );
+}
---
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import "./sidebar.scss";
+
+
+export default function Sidebar({ children }) {
+  return (
+    <div>
+        Sidebar
+    </div>
+  );
+}
